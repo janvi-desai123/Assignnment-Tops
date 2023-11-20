df602bdf673e05b21c2f279f7cc8ac4530979a62
janvi-desai123 <138958784+janvi-desai123@users.noreply.github.com>
2023-11-20 16:43:37 +0530

    Add files via upload

---
+++ C/area of rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int l,b,area;
+	printf("enter length and width");
+	scanf("%d %d",&l,&b);
+	area=1*b;
+	printf("length is %d and width is %d \nso area is %d",l,b,area);
+	
+	
+	return 0;
+}
---
+++ C/area of square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int side,area;
+	printf("enter side");
+	scanf("%d",&side);
+	area=side*side;
+	printf("one side of square is %d \n so area of sqaure is %d",side,area);
+	
+	
+	return 0;
+}
