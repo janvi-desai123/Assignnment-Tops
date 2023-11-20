258de2b1927c94176d2e749d79da1080395583c5
janvi-desai123 <138958784+janvi-desai123@users.noreply.github.com>
2023-11-20 16:44:22 +0530

    Add files via upload

---
+++ SE new/C/area of circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	float r,area,pi=3.14;
+	printf("enter radious");
+	scanf("%f",&r);
+	area=pi*r*r;
+	printf("radious is %f \n so area of circle is %f",r,area);
+	
+	
+	
+	
+	return 0;
+}
